16179d2da8cb87c0778a22291ade8b4532b82136
narekaghbalyan <n.aghbalyan07@gmail.com>
2023-02-02 13:37:11 +0400

    Add where clauses

--- QueryBuilder/QueryBuilder.php
+++ QueryBuilder/QueryBuilder.php
@@ -2,6 +2,8 @@
 
 namespace Moarai\QueryBuilder;
 
+use Exception;
+
 class QueryBuilder
 {
     use ClauseBindersToolkit;
@@ -37,5 +39,310 @@ class QueryBuilder
     protected function fromClauseBinder(string $table): void
     {
         $this->bind('from', [$this->concludeGraveAccent($table)]);
+    }
+
+    /*
+     * where('column', '=', 'value') -> where column = value
+     * where(['column', '=', 'value']) -> where column = value
+     * where(['column' => 'value']) -> where column = value
+     * where(['column1' => 'value1', 'column2' => 'value2']) -> where column1 = value1 and column2 = value2
+     * where(function ($query) { $query->... }, '=', value) -> where $query->result = value
+     */
+    protected function baseConditionClauseBinder(string $whereLogicalType,
+                                                 string $conditionType,
+                                                 string|array|callable $column,
+                                                 string|null $operator,
+                                                 string $value): void
+    {
+        if (is_array($column)) {
+            if (!is_null($operator) || !empty($value)) {
+                throw new Exception(
+                    'If for the "' . $conditionType . '" function the first argument is passed as an array, 
+                    then the following arguments must be omitted.'
+                );
+            }
+
+            if ($this->isAssociative($column)) {
+                $keys = array_keys($column);
+                $columnFirstElementKey = $keys[0];
+
+                if (count($column) === 1) {
+                    $value = array_pop($column);
+
+                    $column = $columnFirstElementKey;
+
+                    $this->throwExceptionIfMisplacedArray($column);
+
+                    $this->bind($conditionType, [
+                        $whereLogicalType,
+                        $this->concludeGraveAccent($column),
+                        '=',
+                        $value
+                    ]);
+                } elseif (count($column) > 1) {
+                    foreach ($column as $columnName => $columnValue) {
+                        $this->throwExceptionIfMisplacedArray($columnValue);
+
+                        if ($columnName === $columnFirstElementKey) {
+                            $this->bind($conditionType, [
+                                $whereLogicalType,
+                                $this->concludeGraveAccent($columnName),
+                                '=',
+                                $columnValue
+                            ]);
+                        } else {
+                            $this->bind($conditionType, [
+                                'and',
+                                $this->concludeGraveAccent($columnName),
+                                '=',
+                                $columnValue
+                            ]);
+                        }
+                    }
+                }
+            } else {
+                if (count($column) === 3) {
+                    foreach ($column as $columnValue) {
+                        $this->throwExceptionIfMisplacedArray($columnValue);
+                    }
+
+                    $this->checkOperatorMatching($column[1]);
+
+                    $this->bind($conditionType, [
+                        $whereLogicalType,
+                        $this->concludeGraveAccent($column[0]),
+                        $column[1],
+                        $column[2]
+                    ]);
+                } else {
+                    throw new Exception(
+                        'Invalid data for "' . $conditionType . '" clause.'
+                    );
+                }
+            }
+        } elseif (is_string($column)) {
+            if (empty($value)) {
+                if (!$this->checkMatching($operator, $this->operators)) {
+                    $value = $operator;
+                    $operator = '=';
+                } else {
+                    throw new Exception(
+                        'Missing argument in "' . $conditionType . '" function.'
+                    );
+                }
+            } else {
+                $this->checkOperatorMatching($operator);
+            }
+
+            $this->bind($conditionType, [
+                $whereLogicalType,
+                $this->concludeGraveAccent($column),
+                $operator,
+                $value
+            ]);
+        } elseif (is_callable($column)) {
+            if (is_null($operator) || empty($value)) {
+                throw new Exception(
+                    'Missing argument in "' . $conditionType . '" function.'
+                );
+            }
+
+            $this->bind($conditionType, [$whereLogicalType]);
+
+            $this->runCallbackForVirginInstance($conditionType, $column);
+
+            $this->bind($conditionType, [
+                $operator,
+                $value
+            ]);
+        }
+    }
+
+    protected function whereBetweenClauseBinder(string $whereLogicalType,
+                                                string|callable $column,
+                                                array|string|int|float $range,
+                                                string|int|float $endOfRange,
+                                                bool $isNotCondition = false,
+                                                bool $betweenColumns = false): void
+    {
+        if (!is_callable($column)) {
+            if (is_array($range)) {
+                $this->throwExceptionIfArrayAssociative(
+                    $range,
+                    'Array for range cannot be associative.'
+                );
+
+                if (count($range) !== 2) {
+                    throw new Exception(
+                        'Array for range must contain 2 elements. 
+                    The first value is the beginning of the range, the second value is the end of the range.'
+                    );
+                }
+
+                if (!empty($endOfRange)) {
+                    throw new Exception(
+                        'If the range is specified as an array, then the third argument of the function must be skipped.'
+                    );
+                }
+
+                if ($betweenColumns) {
+                    $startOfRange = '(select ' . $range[0] . ')';
+                    $endOfRange = '(select ' . $range[1] . ')';
+                } else {
+                    $startOfRange = $range[0];
+                    $endOfRange = $range[1];
+                }
+            } else {
+                if (empty($endOfRange)) {
+                    throw new Exception('Range end not specified');
+                }
+
+                $startOfRange = $range;
+            }
+
+            $this->bind('where', [
+                $whereLogicalType,
+                $this->concludeGraveAccent($column),
+                $isNotCondition ? 'not' : '',
+                'between',
+                $startOfRange . ' and ' . $endOfRange
+            ]);
+        } else {
+            $this->runCallback(
+                'where',
+                $whereLogicalType,
+                $column
+            );
+        }
+    }
+
+    protected function whereColumnClauseBinder(string $whereLogicalType,
+                                               string|array $firstColumn,
+                                               string|null $operator,
+                                               string|null $secondColumn,
+                                               bool $isNotCondition = false): void
+    {
+        if (is_string($firstColumn)) {
+            if (empty($operator) || empty($secondColumn)) {
+                throw new Exception(
+                    'Missing argument in where column function.'
+                );
+            }
+
+            $this->checkOperatorMatching($operator);
+        } elseif (is_array($firstColumn)) {
+            if (!empty($operator) || !empty($secondColumn)) {
+                throw new Exception(
+                    'If the first argument is an array, then the following arguments must be omitted.'
+                );
+            }
+
+            if (count($firstColumn) !== 3) {
+                throw new Exception('Array must contain 3 elements.');
+            }
+
+            if ($this->isAssociative($firstColumn)) {
+                throw new Exception('Array cannot be associative.');
+            }
+
+            $columns = $firstColumn;
+
+            $firstColumn = $columns[0];
+            $operator = $columns[1];
+            $secondColumn = $columns[2];
+
+            $this->checkOperatorMatching($operator);
+        }
+
+        $this->bind('where', [
+            $whereLogicalType,
+            $isNotCondition ? 'not' : '',
+            $this->concludeGraveAccent($firstColumn),
+            $operator,
+            $this->concludeGraveAccent($secondColumn),
+        ]);
+    }
+
+    protected function whereExistsClauseBinder(string $whereLogicalType,
+                                               callable $callback,
+                                               bool $isNotCondition = false)
+    {
+        $this->bind('where', [
+            $whereLogicalType,
+            $isNotCondition ? 'not' : '',
+            'exists'
+        ]);
+
+        $this->runCallbackForVirginInstance(
+            'where',
+            $callback
+        );
+    }
+
+    protected function whereFullTextClauseBinder(string $whereLogicalType,
+                                                 string $column,
+                                                 string $value,
+                                                 bool $isNotCondition = false): void
+    {
+        $this->bind('where', [
+            $whereLogicalType,
+            $isNotCondition ? 'not' : '',
+            'match',
+            $this->concludeGraveAccent($column),
+            'against (',
+            $value,
+            'in natural language mode)'
+        ]);
+    }
+
+    protected function whereInClauseBinder(string $whereLogicalType,
+                                           string|callable $column,
+                                           array $setOfSupposedVariables,
+                                           bool $isNotCondition = false): void
+    {
+        if (!is_callable($column)) {
+            $this->throwExceptionIfArrayAssociative(
+                $setOfSupposedVariables,
+                'Array for variables cannot be associative'
+            );
+
+            if (empty($setOfSupposedVariables)) {
+                throw new Exception('Array for values cannot be empty');
+            }
+
+            $this->bind('where', [
+                $whereLogicalType,
+                $this->concludeGraveAccent($column),
+                $isNotCondition ? 'not' : '', 'in',
+                '(' . implode(', ', $setOfSupposedVariables) . ')'
+            ]);
+        } else {
+            $this->runCallback(
+                'where',
+                $whereLogicalType,
+                $column
+            );
+        }
+    }
+
+    protected function whereNullClauseBinder(string $whereLogicalType,
+                                             string|callable $column,
+                                             bool $isNotCondition = false)
+    {
+        if (!is_callable($column)) {
+            $this->bind('where', [
+                $whereLogicalType,
+                $this->concludeGraveAccent($column),
+                'is',
+                $isNotCondition ? 'not' : '',
+                'null'
+            ]);
+        } else {
+            $this->runCallback(
+                'where',
+                $whereLogicalType,
+                $column
+            );
+        }
     }
 }
--- QueryBuilder/QueryBuilderRepresentativeSpokesman.php
+++ QueryBuilder/QueryBuilderRepresentativeSpokesman.php
@@ -31,4 +31,60 @@ class QueryBuilderRepresentativeSpokesman extends QueryBuilder
 
         return $this;
     }
+
+    public function where(string|array|callable $column, string|null $operator = null, string|null $value = ''): self
+    {
+        $this->baseConditionClauseBinder('', 'where', $column, $operator, $value);
+
+        return $this;
+    }
+
+    public function whereBetween(string|callable $column, array|string|int|float $range = '', string|int|float $endOfRange = ''): self
+    {
+        $this->whereBetweenClauseBinder('', $column, $range, $endOfRange);
+
+        return $this;
+    }
+
+    public function whereBetweenColumns(string|callable $column, array|string|int|float $range = '', string|int|float $endOfRange = ''): self
+    {
+        $this->whereBetweenClauseBinder('', $column, $range, $endOfRange, false, true);
+
+        return $this;
+    }
+
+    public function whereIn(string|callable $column, array $setOfSupposedVariables = []): self
+    {
+        $this->whereInClauseBinder('', $column, $setOfSupposedVariables);
+
+        return $this;
+    }
+
+    public function whereNull(string|callable $column): self
+    {
+        $this->whereNullClauseBinder('', $column, false);
+
+        return $this;
+    }
+
+    public function whereExists(callable $callback): self
+    {
+        $this->whereExistsClauseBinder('', $callback);
+
+        return $this;
+    }
+
+    public function whereColumn(string|array $firstColumn, string|null $operator = null, string|null $secondColumn = null): self
+    {
+        $this->whereColumnClauseBinder('', $firstColumn, $operator, $secondColumn);
+
+        return $this;
+    }
+
+    public function whereFullText(string $column, string $value): self
+    {
+        $this->whereFullTextClauseBinder('', $column, $value, false);
+
+        return $this;
+    }
 }
