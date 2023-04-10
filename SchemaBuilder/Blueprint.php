03498ba4370822c2db492e19681d7a25b31fba44
narekaghbalyan <n.aghbalyan07@gmail.com>
2023-04-10 14:35:16 +0400

    Add blueprint entity

---
+++ SchemaBuilder/Blueprint.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Moarai\SchemaBuilder;
+
+class Blueprint
+{
+
+}
--- SchemaBuilder/Schema.php
+++ SchemaBuilder/Schema.php
@@ -2,7 +2,13 @@
 
 namespace Moarai\SchemaBuilder;
 
+use Closure;
+
 class Schema extends SchemaBuilder
 {
-
+    public static function create(string $table, Closure $callback)
+    {
+        dump($callback);
+        dd($table);
+    }
 }
